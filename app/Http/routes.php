ba454e373f54c9f64dd49237398a7272a807e249
Mohammad Selim Miah <selimcse98@gmail.com>
2016-07-25 03:27:31 +1000

    Step by step process added at readme.md

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,6 +4,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(['prefix'=>'api','middleware'=>'auth:api'], function(){
+    Route::resource('note','NoteController');
+});
+
 Route::group(['middleware'=>'web'], function (){
 
     Route::auth();
